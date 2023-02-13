be60ac250ddfe55a2aeb15c3f04f7885aeb3cb0f
furkanGit <furkancaliskan9801@gmail.com>
2023-02-13 14:47:22 +0100

    first commit

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+
+use App\Models\Person;
+
+class MainController extends Controller
+{
+    public function home()
+    {
+
+        $people = Person::all();
+
+        return view('pages.home', compact('people'));
+    }
+}
