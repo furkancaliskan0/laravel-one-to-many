1450f0744cab485df58c760e66dee65ee7e0ec4c
furkanGit <furkancaliskan9801@gmail.com>
2023-02-13 17:14:56 +0100

    created post seeders and add posts route

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\MainController;
 
+
 Route::get('/', [MainController::class, 'home'])
     ->name('home');
+Route::get('/posts', [MainController::class, 'posts'])
+    ->name('posts');
